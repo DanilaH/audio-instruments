0a591ae0c217316fd466d97f1139f53fec9b52e4
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-30 16:33:53 +0500

    P8.3: derive robots policy from Astro site config

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,18 @@
+import type { APIRoute } from "astro";
+
 import {
   buildRobotsTxt,
-  resolveSiteIndexingConfig,
+  resolveSiteIndexingConfigFromSite,
 } from "../config/site-indexing";
 
 export const prerender = true;
 
-export function GET(): Response {
-  const indexing = resolveSiteIndexingConfig(import.meta.env);
+export const GET: APIRoute = ({ site }) => {
+  const indexing = resolveSiteIndexingConfigFromSite(site);
 
   return new Response(buildRobotsTxt(indexing), {
     headers: {
       "Content-Type": "text/plain; charset=utf-8",
     },
   });
-}
+};
