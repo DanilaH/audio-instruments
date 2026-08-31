2d04207b010ff765a97c905fe1dcba8e16aafd4f
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-31 20:52:19 +0500

    P8: make spectrum construction harness serialization-safe

--- tests/browser/spectrum-analyzer-construction-failure.spec.ts
+++ tests/browser/spectrum-analyzer-construction-failure.spec.ts
@@ -32,18 +32,18 @@ test("partial analyzer construction is disposed before a clean retry", async ({ 
     }
 
     class FakeAnalyserNode extends FakeNode {
-      #fftSize = 2_048;
+      _fftSize = 2_048;
       smoothingTimeConstant = 0;
       minDecibels = -100;
       maxDecibels = -30;
       get fftSize() {
-        return this.#fftSize;
+        return this._fftSize;
       }
       set fftSize(value: number) {
-        this.#fftSize = value;
+        this._fftSize = value;
       }
       get frequencyBinCount() {
-        return this.#fftSize / 2;
+        return this._fftSize / 2;
       }
       getFloatTimeDomainData(target: Float32Array) {
         target.fill(0.1);
