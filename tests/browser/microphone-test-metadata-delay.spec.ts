8dec55f0ebfdf96a8b579cf8e45220ac6d1a6e37
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-31 20:51:55 +0500

    P8: make microphone metadata harness serialization-safe

--- tests/browser/microphone-test-metadata-delay.spec.ts
+++ tests/browser/microphone-test-metadata-delay.spec.ts
@@ -28,18 +28,18 @@ test("slow device enumeration never blocks active capture state or Stop", async 
     }
 
     class FakeAnalyserNode extends FakeNode {
-      #fftSize = 2_048;
+      _fftSize = 2_048;
       smoothingTimeConstant = 0;
       minDecibels = -100;
       maxDecibels = -30;
       get fftSize() {
-        return this.#fftSize;
+        return this._fftSize;
       }
       set fftSize(value: number) {
-        this.#fftSize = value;
+        this._fftSize = value;
       }
       get frequencyBinCount() {
-        return this.#fftSize / 2;
+        return this._fftSize / 2;
       }
       getFloatTimeDomainData(target: Float32Array) {
         target.fill(0.1);
