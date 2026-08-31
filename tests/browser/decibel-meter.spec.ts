4d69222614c516aec70cde67a3a7bf1eb53c0a99
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-31 14:39:43 +0500

    test: keep decibel analyser harness self-contained

--- tests/browser/decibel-meter.spec.ts
+++ tests/browser/decibel-meter.spec.ts
@@ -54,19 +54,19 @@ async function installDbHarness(page: Page): Promise<void> {
     }
 
     class FakeAnalyserNode extends FakeNode {
-      #fftSize = 2_048;
+      fftSizeValue = 2_048;
       smoothingTimeConstant = 0;
       minDecibels = -100;
       maxDecibels = -30;
 
       get fftSize() {
-        return this.#fftSize;
+        return this.fftSizeValue;
       }
       set fftSize(value: number) {
-        this.#fftSize = value;
+        this.fftSizeValue = value;
       }
       get frequencyBinCount() {
-        return this.#fftSize / 2;
+        return this.fftSizeValue / 2;
       }
       getFloatTimeDomainData(target: Float32Array) {
         state.meterReadTimes.push(performance.now());
